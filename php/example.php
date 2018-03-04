9af6d6cb928594b4dfd7785c9da50581e6d2fed7
Rasmus Koit <rasmuskoit@gmail.com>
2018-03-04 20:38:44 +0200

    Testing Example

---
+++ php/example.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: raks4
+ * Date: 04.03.2018
+ * Time: 19:50
+ */
+
+require __DIR__ . '../SourceQuery/bootstrap.php';
+use xPaw\SourceQuery\SourceQuery;
+
+
+// Edit this ->
+define( 'SQ_SERVER_ADDR', '172.20.20.27' );
+define( 'SQ_SERVER_PORT', 27015 );
+define( 'SQ_TIMEOUT',     1 );
+define( 'SQ_ENGINE',      SourceQuery::SOURCE );
+// Edit this <-
+
+$Timer = MicroTime( true );
+
+$Query = new SourceQuery( );
+
+$Info    = Array( );
+$Players = Array( );
+
+try
+{
+    $Query->Connect( SQ_SERVER_ADDR, SQ_SERVER_PORT, SQ_TIMEOUT, SQ_ENGINE );
+    //$Query->SetUseOldGetChallengeMethod( true ); // Use this when players/rules retrieval fails on games like Starbound
+    $Info    = $Query->GetInfo( );
+    $Players = $Query->GetPlayers( );
+
+
+
+
+}
+catch( Exception $e )
+{
+    $Exception = $e;
+}
+finally
+{
+    $Query->Disconnect( );
+}
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="utf-8">
+    <title>CSGO Manager</title>
+    <link rel="stylesheet" href="../css/style.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Cabin:600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1>CS GO Manager!</h1>
+    <span>Counter-Strike Global Offensive Server Manager</span>
+</header>
+<?php
+
+
+print $Info['HostName'];
+print SQ_SERVER_ADDR;
+print $Info['GamePort'];
+print $Info['Players'];
+?>
+<form>
+    <table class="form-table">
+        <tbody>
+        <tr>
+            <td>Server name</td>
+            <td><?php print $Info['HostName']?></td>
+        </tr>
+        <tr>
+            <td>Perekonnanimi:</td>
+            <td><input name="lastName" value=""></td>
+        </tr>
+        <tr>
+            <td>Telefonid:</td>
+            <td><input name="phone1" value=""></td>
+        </tr>
+        <tr>
+            <td colspan="2"><br>
+                <input name="submit-button" type="submit" value="Salvesta">
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
